145c04e47960a1696f22c83b5d5df63ce178066d
Graeme Winter <graeme.winter@gmail.com>
2021-03-31 15:34:38 +0100

    Do something with pixels

--- baseline/miniapp.c
+++ baseline/miniapp.c
@@ -13,5 +13,24 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
+    size_t n_images = get_number_of_images();
+
+    for (size_t j = 0; j < n_images; j++) {
+      image_t image = get_image(j);
+
+      size_t zero = 0;
+      for (size_t i = 0; i < (image.fast * image.slow); i++) {
+	if (image.data[i] == 0 && image.mask[i] == 1) {
+	  zero ++;
+	}
+      }
+      printf("image %ld had %ld valid zero pixels\n", j, zero);
+	
+      
+      free_image(image);
+    }
+
+
+    
     return 0;
 }
