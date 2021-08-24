7d734ff14c18709991b32be8fa40aadca56f5a2a
Nicholas Devenish <ndevenish@gmail.com>
2021-08-24 13:23:49 +0100

    Use new FPGA module

--- cmake/Modules/FindFPGA.cmake
+++ cmake/Modules/FindFPGA.cmake
@@ -9,6 +9,53 @@ include (FindPackageHandleStandardArgs)
 
 check_cxx_compiler_flag(-fintelfpga CXX_HAS_FPGA_FLAG)
 
+## Duplicate a target and all relevant properties
+##
+## This is an INTERNAL function to FindFPGA. It doesn't attempt to
+## perfectly copy all properties, as WIP development is ongoing the
+## more important properties might be expanded.
+function(_duplicate_target new_target target)
+    get_target_property(_type ${target} TYPE)
+    if(_type STREQUAL STATIC_LIBRARY)
+        add_library(${new_target} STATIC )
+    elseif(_type STREQUAL MODULE_LIBRARY)
+        add_library(${new_target} MODULE )
+    elseif(_type STREQUAL SHARED_LIBRARY)
+        add_library(${new_target} SHARED )
+    elseif(_type STREQUAL EXECUTABLE)
+        add_executable(${new_target} )
+    endif()
+
+
+    set(copy_properties "")
+    # Build list of namespaced properties
+    foreach(namespace "" INTERFACE)
+        set(_prefix "")
+        if(namespace)
+            set(_prefix "${namespace}_")
+        endif()
+        foreach(_prop
+            COMPILE_DEFINITIONS COMPILE_FEATURES COMPILE_OPTIONS INCLUDE_DIRECTORIES
+            LINK_DEPENDS LINK_DIRECTORIES LINK_LIBRARIES LINK_OPTIONS
+            POSITION_INDEPENDENT_CODE PRECOMPILE_HEADERS SOURCES SYSTEM_INCLUDE_DIRECTORIES
+            )
+            set(_pro "${_prefix}${_prop}")
+            list(APPEND copy_properties "${_prop}")
+        endforeach()
+    endforeach()
+    # "Other" properties
+    # list(APPEND copy_properties LOCATION)
+
+    # Now, copy all the propertes to the new target
+    foreach(_property ${copy_properties})
+        get_target_property(_prop ${target} ${_property})
+        # Copy anything as long as it is not-NOTFOUND
+        if(NOT _prop MATCHES ".*-NOTFOUND$")
+            set_target_properties(${new_target} PROPERTIES ${_property} "${_prop}")
+        endif()
+    endforeach()
+endfunction()
+
 if(CXX_HAS_FPGA_FLAG)
     set(CXX_HAS_FPGA_FLAG Yes)
     # FPGA board selection
@@ -20,6 +67,11 @@ if(CXX_HAS_FPGA_FLAG)
     endif()
     message(STATUS "Configuring for FPGA board: ${FPGA_BOARD}")
 
+    add_custom_target(fpga)
+    set_target_properties(fpga PROPERTIES EXCLUDE_FROM_ALL yes)
+
+    add_custom_target(fpga_report)
+    set_target_properties(fpga_report PROPERTIES EXCLUDE_FROM_ALL yes)
 
     add_library(FPGA::EMULATED INTERFACE IMPORTED )
     set_target_properties(FPGA::EMULATED PROPERTIES
@@ -32,6 +84,37 @@ if(CXX_HAS_FPGA_FLAG)
         INTERFACE_COMPILE_DEFINITIONS "FPGA"
         INTERFACE_COMPILE_OPTIONS "-fintelfpga"
         INTERFACE_LINK_OPTIONS "-fintelfpga;-Xshardware;-Xsboard=${FPGA_BOARD}")
+    
+    ## Add FPGA variants of a target
+    ##
+    ## This will duplicate the target, and add variants for:
+    ## - target.fpga:       Hardware FPGA (Adds -DFPGA compile definition)
+    ## - target.fpga_emu:   Emulated FPGA (Adds -DFPGA and -DFPGA_EMULATOR compile definition)
+    ## - target_report.a:   FPGA Report (Hardware FPGA, but only the early linking stages and report output)
+    function(add_fpga_variants target)
+        get_target_property(_imported ${target} IMPORTED)
+        if(_imported)
+            # Our duplicate target function doesn't handle this yet
+            message(SEND_ERROR "Cannot add FPGA to an imported target")
+        endif()
+
+        # FPGA Emulator
+        _duplicate_target(${target}.fpga_emu ${target})
+        target_link_libraries(${target}.fpga_emu FPGA::EMULATED)
+        
+        # FPGA Report
+        _duplicate_target(${target}_report.a ${target})
+        target_link_libraries(${target}_report.a FPGA::FPGA)
+        target_link_options(${target}_report.a PRIVATE "-fsycl-link=early")
+        set_target_properties(${target}_report.a PROPERTIES EXCLUDE_FROM_ALL yes)
+        add_dependencies(fpga_report ${target}_report.a)
+    
+        # FPGA hardware build
+        _duplicate_target(${target}.fpga ${target})
+        target_link_libraries(${target}.fpga FPGA::FPGA)
+        set_target_properties(${target}.fpga PROPERTIES EXCLUDE_FROM_ALL yes)
+        add_dependencies(fpga ${target}.fpga)
+    endfunction()
 endif()
 
 find_package_handle_standard_args(FPGA REQUIRED_VARS FPGA_BOARD CXX_HAS_FPGA_FLAG)
