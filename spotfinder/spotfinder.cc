1cec4dc54ba356192b9a6dbb6b5663f2e37dc7bd
Nicholas Devenish <ndevenish@gmail.com>
2023-03-17 12:44:30 +0000

    Handle double-SIGINT

--- spotfinder/spotfinder.cc
+++ spotfinder/spotfinder.cc
@@ -33,9 +33,15 @@ using namespace std::chrono_literals;
 // Global stop token for picking up user cancellation
 std::stop_source global_stop;
 
+// Function for passing to std::signal to register the stop request
 extern "C" void stop_processing(int sig) {
-    print("Running interrupted by user request\n");
-    global_stop.request_stop();
+    if (global_stop.stop_requested()) {
+        // We already requested before, but we want it faster. Abort.
+        std::quick_exit(1);
+    } else {
+        print("Running interrupted by user request\n");
+        global_stop.request_stop();
+    }
 }
 
 template <typename T>
