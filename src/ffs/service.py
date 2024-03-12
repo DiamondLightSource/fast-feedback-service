e515534d166ce3a3f2beb87d77904a9c28e4484e
Nicholas Devenish <ndevenish@gmail.com>
2024-03-12 16:34:38 +0000

    Add a basic zocalo service implementation

---
+++ src/ffs/service.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from pprint import pprint, pformat
+
+import workflows.recipe
+from workflows.services.common_service import CommonService
+
+DEFAULT_QUEUE_NAME = "per_image_analysis.gpu"
+
+
+class GPUPerImageAnalysis(CommonService):
+    _service_name = "GPU Per-Image-Analysis"
+    _logger_name = "spotfinder.service"
+
+    def initializing(self):
+        # self.log.debug("Checking Node GPU capabilities")
+        # TODO: Write node sanity checks
+        workflows.recipe.wrap_subscribe(
+            self._transport,
+            self._environment.get("queue") or DEFAULT_QUEUE_NAME,
+            self.gpu_per_image_analysis,
+            acknowledgement=True,
+            log_extender=self.extend_log,
+        )
+
+    def gpu_per_image_analysis(
+        self, rw: workflows.recipe.RecipeWrapper, header: dict, message: dict
+    ):
+        self.log(
+            f"Gotten PIA request:\nPayload:\n {pformat(rw.payload)}\nRecipe: {pformat(rw.recipe.recipe)}"
+        )
