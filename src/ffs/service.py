9298d83ac6a5c52bb72bd77a6fc53d6d814fcf3a
Nicholas Devenish <ndevenish@gmail.com>
2024-03-12 17:19:39 +0000

    Use rich logging in service

--- src/ffs/service.py
+++ src/ffs/service.py
@@ -1,11 +1,29 @@
 from __future__ import annotations
 
+import logging
 from pprint import pformat
 
 import workflows.recipe
+from rich.logging import RichHandler
 from workflows.services.common_service import CommonService
 
 DEFAULT_QUEUE_NAME = "per_image_analysis.gpu"
+
+
+def _setup_rich_logging(level=logging.DEBUG):
+    """Setup a rich-based logging output. Using for debug running."""
+    rootLogger = logging.getLogger()
+
+    for handler in list(rootLogger.handlers):
+        # We want to replace the streamhandler
+        if isinstance(handler, logging.StreamHandler):
+            rootLogger.handlers.remove(handler)
+        # We also want to lower the output level
+        handler.setLevel(rootLogger.level)
+
+    rootLogger.handlers.append(
+        RichHandler(level=level, log_time_format="[%Y-%m-%d %H:%M:%S]")
+    )
 
 
 class GPUPerImageAnalysis(CommonService):
@@ -13,6 +31,7 @@ class GPUPerImageAnalysis(CommonService):
     _logger_name = "spotfinder.service"
 
     def initializing(self):
+        _setup_rich_logging()
         # self.log.debug("Checking Node GPU capabilities")
         # TODO: Write node sanity checks
         workflows.recipe.wrap_subscribe(
