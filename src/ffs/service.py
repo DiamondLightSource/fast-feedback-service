90179cea2474d5d27ac436d67b52daf6c3bbe6f1
Nicholas Devenish <ndevenish@gmail.com>
2024-03-13 09:02:53 +0000

    More service implementation

--- src/ffs/service.py
+++ src/ffs/service.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
 import logging
+import subprocess
+import time
+from pathlib import Path
 from pprint import pformat
 
 import workflows.recipe
@@ -8,6 +11,10 @@ from rich.logging import RichHandler
 from workflows.services.common_service import CommonService
 
 DEFAULT_QUEUE_NAME = "per_image_analysis.gpu"
+
+SPOTFINDER = Path(
+    "/dls/science/users/mep23677/cuda/miniapp/cuda/spotfinder/_build/spotfinder"
+)
 
 
 def _setup_rich_logging(level=logging.DEBUG):
@@ -30,6 +37,8 @@ class GPUPerImageAnalysis(CommonService):
     _service_name = "GPU Per-Image-Analysis"
     _logger_name = "spotfinder.service"
 
+    _spotfind_proc: subprocess.Popen | None = None
+
     def initializing(self):
         _setup_rich_logging()
         # self.log.debug("Checking Node GPU capabilities")
@@ -45,6 +54,27 @@ class GPUPerImageAnalysis(CommonService):
     def gpu_per_image_analysis(
         self, rw: workflows.recipe.RecipeWrapper, header: dict, message: dict
     ):
-        self.log(
-            f"Gotten PIA request:\nPayload:\n {pformat(rw.payload)}\nRecipe: {pformat(rw.recipe.recipe)}"
+        self.log.debug(
+            f"Gotten PIA request:\nHeader:\n {pformat(header)}\nPayload:\n {pformat(rw.payload)}"
         )
+
+        # Do sanity checks, then launch spotfinder
+        if not SPOTFINDER.is_file():
+            self.log.error("Could not find spotfinder executable: %s", SPOTFINDER)
+            rw.transport.nack(header)
+
+        # Form the expected path for this dataset
+        expected_path = f"/dev/shm/{rw.payload['filename']}"
+
+        # Create a pipe for comms
+        # TODO: Set up pipes for communication back from process
+        # (pipe_r, pipe_w) = os.pipe()
+
+        # Now run the spotfinder
+        command = [SPOTFINDER, str(expected_path)]
+        self.log.info(f"Running: {' '.join(command)}")
+        start_time = time.monotonic()
+        _result = subprocess.run(command)
+
+        duration = time.monotonic() - start_time
+        self.log.info(f"Analysis complete in {duration:.1} s")
