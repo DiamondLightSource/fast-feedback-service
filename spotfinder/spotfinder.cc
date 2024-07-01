9417a88115048abee9cc11c21f374fac659ed4aa
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-07-01 10:25:35 +0100

    Add stream selection and error detection

--- spotfinder/spotfinder.cc
+++ spotfinder/spotfinder.cc
@@ -140,7 +140,8 @@ void apply_resolution_filtering(PitchedMalloc<uint8_t> mask,
                                 float wavelength,
                                 detector_geometry detector,
                                 float dmin,
-                                float dmax) {
+                                float dmax,
+                                cudaStream_t stream = 0) {
     // Define the block size and grid size for the kernel
     dim3 threadsPerBlock(32, 32);
     dim3 numBlocks((width + threadsPerBlock.x - 1) / threadsPerBlock.x,
@@ -156,7 +157,9 @@ void apply_resolution_filtering(PitchedMalloc<uint8_t> mask,
                                 .dmax = dmax};
 
     // Launch the kernel to apply resolution filtering
-    call_apply_resolution_mask(numBlocks, threadsPerBlock, 0, 0, mask.get(), params);
+    call_apply_resolution_mask(numBlocks, threadsPerBlock, 0, stream, mask.get(), params);
+
+    CUDA_CHECK(cudaStreamSynchronize(stream));
 }
 
 /// Handle setting up an NppStreamContext from a specific stream
