1b2d4f639dfe2c86c579ab9a194df4f0866696e9
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-10-11 15:45:58 +0100

    Commented unecessary info in test

--- tests/res_comparison.sh
+++ tests/res_comparison.sh
@@ -10,25 +10,32 @@ mkdir -p _debug_output
 cd _debug_output
 
 # Create a JSON file
-cat << EOF > detector.json
-{
-    "pixel_size_x": 0.075,
-    "pixel_size_y": 0.075,
-    "distance": 150,
-    "beam_center_x": 1008.13,
-    "beam_center_y": 1066.0
-}
-EOF
+# cat << EOF > detector.json
+# {
+#     "pixel_size_x": 0.075,
+#     "pixel_size_y": 0.075,
+#     "distance": 150,
+#     "beam_center_x": 1008.13,
+#     "beam_center_y": 1066.0
+# }
+# EOF
 
 # Open file descriptor 3 for writing to output_file.txt
 exec 3> output_file.txt
 
 # Run the program with file descriptor 3
+# ../bin/spotfinder /dls/i03/data/2024/cm37235-2/xraycentring/TestInsulin/ins_14/ins_14_24.nxs \
+#   --min-spot-size 3 \
+#   --pipe_fd 3 \
+#   --dmin 4 \
+#   --detector "$(cat detector.json)" \
+#   --threads 1 \
+#   --images 1
+
 ../bin/spotfinder /dls/i03/data/2024/cm37235-2/xraycentring/TestInsulin/ins_14/ins_14_24.nxs \
   --min-spot-size 3 \
   --pipe_fd 3 \
   --dmin 4 \
-  --detector "$(cat detector.json)" \
   --threads 1 \
   --images 1
 
@@ -36,4 +43,4 @@ exec 3> output_file.txt
 exec 3>&-
 
 # Delete the JSON file
-rm detector.json
+# rm detector.json
