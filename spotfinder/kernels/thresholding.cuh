8975ceeb6922d22c42f2fcd27b6f58d18fe5de88
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-10-21 10:56:49 +0100

    Break thresholding kernels into separate file

---
+++ spotfinder/kernels/thresholding.cuh
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "../spotfinder.cuh"
+
+__global__ void compute_threshold_kernel(pixel_t *image,
+                                         uint8_t *mask,
+                                         uint8_t *result_mask,
+                                         size_t image_pitch,
+                                         size_t mask_pitch,
+                                         size_t result_pitch,
+                                         int width,
+                                         int height,
+                                         pixel_t max_valid_pixel_value,
+                                         int kernel_width,
+                                         int kernel_height,
+                                         int min_count,
+                                         float n_sig_b,
+                                         float n_sig_s);
+
+__global__ void compute_dispersion_threshold_kernel(pixel_t *image,
+                                                    uint8_t *mask,
+                                                    uint8_t *result_mask,
+                                                    size_t image_pitch,
+                                                    size_t mask_pitch,
+                                                    size_t result_pitch,
+                                                    int width,
+                                                    int height,
+                                                    pixel_t max_valid_pixel_value,
+                                                    int kernel_width,
+                                                    int kernel_height,
+                                                    int min_count,
+                                                    float n_sig_b,
+                                                    float n_sig_s);
+
+__global__ void compute_final_threshold_kernel(pixel_t *image,
+                                               uint8_t *mask,
+                                               uint8_t *dispersion_mask,
+                                               uint8_t *result_mask,
+                                               size_t image_pitch,
+                                               size_t mask_pitch,
+                                               size_t dispersion_mask_pitch,
+                                               size_t result_mask_pitch,
+                                               int width,
+                                               int height,
+                                               pixel_t max_valid_pixel_value,
+                                               int kernel_width,
+                                               int kernel_height,
+                                               float n_sig_s,
+                                               float threshold);
