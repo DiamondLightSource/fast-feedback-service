fd7a16daa8224bed44bfc88ba8f4316dad0fc23e
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-11-15 12:40:39 +0000

    Add device_common.cuh for common device code

---
+++ include/device_common.cuh
@@ -0,0 +1,13 @@
+/**
+ * @file device_common.hu
+ * @brief Common device functions
+ */
+
+#ifndef DEVICE_COMMON_H
+#define DEVICE_COMMON_H
+
+#include <cooperative_groups.h>
+#include <cooperative_groups/reduce.h>
+#include <cuda_runtime.h>
+
+#endif  // DEVICE_COMMON_H
