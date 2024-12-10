c75b04141373e2494744be80a2efc5b6f6b3baa0
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2024-12-10 16:53:55 +0000

    Add oscillation retrieval functions to H5Read and Reader interface

--- spotfinder/cbfread.hpp
+++ spotfinder/cbfread.hpp
@@ -155,6 +155,9 @@ class CBFRead : public Reader {
     virtual std::optional<float> get_detector_distance() const {
         return std::nullopt;
     };
+    virtual std::optional<std::array<float, 2>> get_oscillation() const {
+        return std::nullopt;
+    };
 };
 
 template <typename Tout>
