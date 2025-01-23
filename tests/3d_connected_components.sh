4db292e84c56ab0fa01565eaf34b2ca7efc18010
Dimitri Vlachos <dimitri.j.vlachos@gmail.com>
2025-01-23 17:40:11 +0000

    Set and reset log level for test

--- tests/3d_connected_components.sh
+++ tests/3d_connected_components.sh
@@ -9,6 +9,17 @@ mkdir -p _debug_output
 # Change to the output directory
 cd _debug_output
 
+# Save the current value of the environment variable if it exists
+if [ -z "${LOG_LEVEL+x}" ]; then
+  original_value_set=false
+else
+  original_value_set=true
+  original_value=$LOG_LEVEL
+fi
+
+# Change the log level to debug
+export LOG_LEVEL="debug"
+
 # Open file descriptor 3 for writing to output_file.txt
 exec 3> output_file.txt
 
@@ -23,6 +34,13 @@ exec 3> output_file.txt
 
 # Close file descriptor 3
 exec 3>&-
+
+# Restore the original value of the environment variable if it was set
+if [ "$original_value_set" = true ]; then
+  export LOG_LEVEL=$original_value
+else
+  unset LOG_LEVEL
+fi
 
 # Read the output from the file
 # output=$(cat output_file.txt)
