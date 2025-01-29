72833c8d4b03ddfe45304080d523b3a5672f6c3a
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2025-01-29 11:11:13 +0000

    Add unit test for xyz_to_rlp

---
+++ baseline_indexer/test_xyz_to_rlp.cc
@@ -0,0 +1,47 @@
+#include "xyz_to_rlp.cc"
+#include <gtest/gtest.h>
+
+#include <dx2/beam.h>
+#include <dx2/detector.h>
+#include <dx2/goniometer.h>
+#include <dx2/scan.h>
+#include <math.h>
+#include <nlohmann/json.hpp>
+#include <Eigen/Dense>
+
+using Eigen::Matrix3d;
+using Eigen::Vector3d;
+using json = nlohmann::json;
+
+TEST(BaselineIndexer, XyztoRlptest) {
+    std::vector<double> xyzobs_px{{10.1,10.1,50.2, 20.1,20.1,70.2}};
+    Scan scan{{1,100}, {0.0,0.1}}; //image range and oscillation.
+    Goniometer gonio{{{1.0,0.0,0.0}}, {{0.0}}, {{"phi"}}, 0}; //axes, angles, names, scan-axis.
+    json panel_data;
+    panel_data["fast_axis"] = {1.0, 0.0, 0.0};
+    panel_data["slow_axis"] = {0.0, -1.0, 0.0};
+    panel_data["origin"] = {-150,162,-200};
+    panel_data["pixel_size"] = {0.075,0.075};
+    panel_data["image_size"] = {4148,4362};
+    panel_data["trusted_range"] = {0.0,46051};
+    panel_data["type"] = std::string("SENSOR_PAD");
+    panel_data["name"] = std::string("test");
+    panel_data["raw_image_offset"] = {0,0};
+    panel_data["thickness"] = 0.45;
+    panel_data["material"] = "Si";
+    panel_data["mu"] = 3.92;
+    panel_data["gain"] = 1.0;
+    panel_data["pedestal"] = 0.0;
+    json pxdata;
+    pxdata["type"] = std::string("ParallaxCorrectedPxMmStrategy");
+    panel_data["px_mm_strategy"] = pxdata;
+    Panel panel{panel_data}; // use defaults
+    MonochromaticBeam beam{1.0}; //wavelength
+    std::vector<Vector3d> rlp = xyz_to_rlp(
+        xyzobs_px, panel, beam, scan, gonio
+    );
+    Vector3d expected_0{{-0.5021752936083477, 0.5690514955867707, 0.27788051106787137}};
+    Vector3d expected_1{{-0.5009709068399325, 0.5770958485799975, 0.2562207980973077}};
+    EXPECT_EQ(rlp[0], expected_0);
+    EXPECT_EQ(rlp[1], expected_1);
+}
