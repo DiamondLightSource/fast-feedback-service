c776881a0cb22eeba6e94d9f824bf8aff76ed6ab
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2025-08-28 11:19:18 +0100

    Add extensive 2D test

--- conftest.py
+++ conftest.py
@@ -8,3 +8,4 @@ import pytest
 def set_env_vars():
     os.environ["INDEXER"] = os.fspath(Path.cwd() / "build/bin/baseline_indexer")
     os.environ["SPOTFINDER"] = os.fspath(Path.cwd() / "build/bin/spotfinder")
+    os.environ["FFS_ROOT_DIR"] = os.fspath(Path.cwd())
