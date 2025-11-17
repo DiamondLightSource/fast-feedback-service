80573796b1caafed59b59c2711c35a789dc90a2e
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2025-11-17 14:47:54 +0000

    Add a baseline predictor for monochromatic rotation data. (#74)
    Add a monochromatic predictor for rotation data.
    
    Co-authored-by: 143278514+yash4karan@users.noreply.github.com

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ import pytest
 def set_env_vars():
     os.environ["INDEXER"] = os.fspath(Path.cwd() / "build/bin/baseline_indexer")
     os.environ["SPOTFINDER"] = os.fspath(Path.cwd() / "build/bin/spotfinder")
+    os.environ["PREDICTOR"] = os.fspath(Path.cwd() / "build/bin/baseline_predictor")
     os.environ["SPOTFINDER_32BIT"] = os.fspath(
         Path.cwd() / "build_32bit/bin/spotfinder"
     )
