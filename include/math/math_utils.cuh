7bc61e986f65d6d6383dcddecdaf1a16c86acaf9
Dimitrios Vlachos <dimitrios.vlachos@diamond.ac.uk>
2025-11-28 16:38:02 +0000

    Refactor math utility to allow for non-CUDA usage

--- include/math/math_utils.cuh
+++ include/math/math_utils.cuh
@@ -14,15 +14,22 @@
  */
 #include <cmath>
 
+#ifdef __CUDACC__
 #include "math/device_precision.cuh"
+#define DEVICE_HOST __device__ __host__
+using scalar_type = scalar_t;
+#else
+#define DEVICE_HOST inline
+using scalar_type = double;
+#endif
 
 /**
  * @brief Convert radians to degrees
  * @param radians Angle in radians
  * @return Angle in degrees
  */
-__device__ __host__ inline scalar_t radians_to_degrees(scalar_t radians) {
-    constexpr scalar_t RAD_TO_DEG = 180.0 / static_cast<scalar_t>(M_PI);
+DEVICE_HOST scalar_type radians_to_degrees(scalar_type radians) {
+    constexpr scalar_type RAD_TO_DEG = 180.0 / static_cast<scalar_type>(M_PI);
     return radians * RAD_TO_DEG;
 }
 
@@ -31,7 +38,9 @@ __device__ __host__ inline scalar_t radians_to_degrees(scalar_t radians) {
  * @param degrees Angle in degrees
  * @return Angle in radians
  */
-__device__ __host__ inline scalar_t degrees_to_radians(scalar_t degrees) {
-    constexpr scalar_t DEG_TO_RAD = static_cast<scalar_t>(M_PI) / 180.0;
+DEVICE_HOST scalar_type degrees_to_radians(scalar_type degrees) {
+    constexpr scalar_type DEG_TO_RAD = static_cast<scalar_type>(M_PI) / 180.0;
     return degrees * DEG_TO_RAD;
 }
+
+#undef DEVICE_HOST
