e5a2897efb007f7649e885f7c7a162e1d90ce1d1
Dimitrios Vlachos <dimitrios.vlachos@diamond.ac.uk>
2025-12-11 10:54:54 +0000

    Add comments to clarify sigma_m and sigma_b parameters in radians

--- integrator/integrator.cc
+++ integrator/integrator.cc
@@ -152,8 +152,10 @@ int main(int argc, char **argv) {
     const auto experiment_file = parser.experiment();
     float wait_timeout = parser.get<float>("timeout");
     // These two will be optional later, should be gettable from reflection table
-    float sigma_m = degrees_to_radians(parser.get<float>("sigma_m"));
-    float sigma_b = degrees_to_radians(parser.get<float>("sigma_b"));
+    float sigma_m =
+      degrees_to_radians(parser.get<float>("sigma_m"));  ///< σm in radians
+    float sigma_b =
+      degrees_to_radians(parser.get<float>("sigma_b"));  ///< σb in radians
 
     // Guard against missing files
     if (!std::filesystem::exists(reflection_file)) {
