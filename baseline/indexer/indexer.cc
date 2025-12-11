deac9ea0c44f28f279fafd09b73d926e5c16ef90
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2025-12-11 09:27:07 +0000

    Validate expected shape of xyzobs.px.value

--- baseline/indexer/indexer.cc
+++ baseline/indexer/indexer.cc
@@ -149,6 +149,10 @@ int main(int argc, char **argv) {
           "No xyzobs.px.value column found in input reflection table.");
     }
     mdspan_type<double> xyzobs_px = xyzobs_px_.value();
+    if (xyzobs_px.rank() != 2 || xyzobs_px.extent(1) != 3){
+      throw std::runtime_error(
+          "Input xyzobs.px.value column does not have the expected shape of (N,3).");
+    }
 
     // The diffraction spots form a lattice in reciprocal space (if the experimental
     // geometry is accurate). So use the experimental models to transform the spot
