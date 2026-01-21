28842facc0de4f0271191d5d3d3db5cabe5d6ea3
Nicholas Devenish <ndevenish@gmail.com>
2026-01-21 13:27:34 +0000

    Update conftest pointer to spotfinder32

--- conftest.py
+++ conftest.py
@@ -9,8 +9,6 @@ def set_env_vars():
     os.environ["INDEXER"] = os.fspath(Path.cwd() / "build/bin/baseline_indexer")
     os.environ["SPOTFINDER"] = os.fspath(Path.cwd() / "build/bin/spotfinder")
     os.environ["PREDICTOR"] = os.fspath(Path.cwd() / "build/bin/baseline_predictor")
-    os.environ["SPOTFINDER_32BIT"] = os.fspath(
-        Path.cwd() / "build_32bit/bin/spotfinder"
-    )
+    os.environ["SPOTFINDER_32BIT"] = os.fspath(Path.cwd() / "build/bin/spotfinder32")
     os.environ["INTEGRATOR"] = os.fspath(Path.cwd() / "build/bin/integrator")
     os.environ["FFS_ROOT_DIR"] = os.fspath(Path.cwd())
