c99924bca70ac5d2da50d4ef625bfdc523f26897
James Beilsten-Edmands <30625594+jbeilstenedmands@users.noreply.github.com>
2026-03-30 19:23:23 +0100

    Output more data items

--- baseline/integrator/integrator.cc
+++ baseline/integrator/integrator.cc
@@ -101,6 +101,7 @@ class BaselineIntegratorArgumentParser : public FFSArgumentParser {
 };
 #pragma endregion Argument Parsing
 constexpr double DEG2RAD = M_PI / 180.0;
+constexpr double RAD2DEG = 180.0 / M_PI;
 class CoordinateSystem {
   public:
     CoordinateSystem(Vector3d m2, Vector3d s0, Vector3d s1, double phi) : s1_(s1), phi_(phi) {
@@ -120,6 +121,9 @@ class CoordinateSystem {
           scaled_e2_.dot(s_dash - s1_),
           zeta_ * (phi_dash - phi_)};
       return coord;
+    }
+    double zeta() const {
+      return zeta_;
     }
   private:
     Vector3d s1_;
@@ -400,6 +404,8 @@ int main(int argc, char **argv) {
     std::vector<int> bg_accumulators(num_reflections);
     std::vector<int> nbg_accumulators(num_reflections);
     std::vector<int> nfg_accumulators(num_reflections);
+    std::vector<int> nbgbad_accumulators(num_reflections);
+    std::vector<bool> success(num_reflections, true);
 
     for (size_t refl_id = 0; refl_id < num_reflections; ++refl_id) {
         const auto &bbox = computed_bounding_boxes[refl_id];
@@ -429,10 +435,9 @@ int main(int argc, char **argv) {
 
     // Now load some image data:
     std::string filename = expt.imagesequence().filename();
-    h5read_handle *obj = h5read_open(filename.c_str());
-    size_t n_images = h5read_get_number_of_images(obj);
-    uint16_t image_slow = h5read_get_image_slow(obj);
-    uint16_t image_fast = h5read_get_image_fast(obj);
+    auto reader = H5Read(filename);
+    size_t n_images = reader.get_number_of_images();
+    auto [image_slow, image_fast] = reader.image_shape();
     double s0_length = beam.get_s0().norm();
     double phi0 = scan.get_oscillation()[0];
     double dphi = scan.get_oscillation()[1];
@@ -449,9 +454,12 @@ int main(int argc, char **argv) {
     double delta_b_r2 = 1.0 / (delta_b * delta_b);
     double delta_m = sigma_m * 3.0;
     double delta_m_r2 = 1.0 / (delta_m * delta_m);
-
+    auto mask = reader.get_mask().value();
     for (size_t j = 0; j < n_images; j++) {
-      image_t *image = h5read_get_image(obj, j);
+      if (!reader.is_image_available(j)) {
+        continue;
+      }
+      auto image = reader.get_image(j);
       std::cout << "Processing image " << j +1 << std::endl;
       size_t zero = 0;
       size_t masked = 0;
@@ -497,22 +505,35 @@ int main(int argc, char **argv) {
           for (int y = bbox.y_min; y<bbox.y_max; y++){
             if (x >=0 && x < image_fast && y >=0 && y < image_slow){
               int index = x + (y * image_fast);
-              if (image->mask[index] != 0){
-                int data = image->data[index];
+              //std::cout << index << std::endl;
+              int x_index = x - bbox.x_min;
+              int y_index = y - bbox.y_min;
+              int dxyindex = x_index + (y_index * n_x);
+              double d1 = dxy_start[dxyindex];
+              double d2 = dxy_start[dxyindex+1];
+              double d3 = dxy_start[dxyindex+n_x];
+              double d4 = dxy_start[dxyindex+n_x+1];
+              double d5 = dxy_end[dxyindex];
+              double d6 = dxy_end[dxyindex+1];
+              double d7 = dxy_end[dxyindex+n_x];
+              double d8 = dxy_end[dxyindex+n_x+1];
+              double d = std::min(std::min(std::min(d1, d2), std::min(d3, d4)),
+                                  std::min(std::min(d5, d6), std::min(d7, d8)));
+              if (mask[index] == 0){
+                // masked, might be ok if bg
+                if (d > 1.0){
+                  nbgbad_accumulators[refl_id] += 1;
+                }
+                else {
+                  success[refl_id] = false;
+                }
+              } else {
+                // Not masked
+                uint16_t data = image.data.data()[index];
+                //std::cout << "Read data" << std::endl;
                 // Need to work out if foreground or background.
-                int x_index = x - bbox.x_min;
-                int y_index = y - bbox.y_min;
-                int dxyindex = x_index + (y_index * n_x);
-                double d1 = dxy_start[dxyindex];
-                double d2 = dxy_start[dxyindex+1];
-                double d3 = dxy_start[dxyindex+n_x];
-                double d4 = dxy_start[dxyindex+n_x+1];
-                double d5 = dxy_end[dxyindex];
-                double d6 = dxy_end[dxyindex+1];
-                double d7 = dxy_end[dxyindex+n_x];
-                double d8 = dxy_end[dxyindex+n_x+1];
-                double d = std::min(std::min(std::min(d1, d2), std::min(d3, d4)),
-                                    std::min(std::min(d5, d6), std::min(d7, d8)));
+                
+                //std::cout << "Calculated d" << std::endl;
                 if (d > 1.0){
                   bg_accumulators[refl_id] += data;
                   nbg_accumulators[refl_id] += 1;
@@ -526,9 +547,10 @@ int main(int argc, char **argv) {
           }
         }
       }
-      h5read_free_image(image);
+      //h5read_free_image(image);
+      //std::cout << "Processed image " << j +1 << std::endl;
     }
-    h5read_free(obj);
+    //h5read_free(obj);
 
     // Convert to reflection table format
     std::vector<double> computed_bbox_data;
@@ -550,24 +572,72 @@ int main(int argc, char **argv) {
     //integrated_data.add_column(
     //  "baseline_bbox", std::vector<size_t>{num_reflections, 6}, computed_bbox_data);
     std::vector<double> intensities(num_reflections);
+    std::vector<double> variances(num_reflections);
+    std::vector<double> partialities(num_reflections);
     for (int i=0;i<num_reflections;i++){
       if (nbg_accumulators[i]){
-        intensities[i] = intensity_accumulators[i] - (nfg_accumulators[i] * bg_accumulators[i] / static_cast<double>(nbg_accumulators[i]));
+        double bg = nfg_accumulators[i] * bg_accumulators[i] / static_cast<double>(nbg_accumulators[i]); // average bg * num fg pixels
+        double I = intensity_accumulators[i] - bg;
+        intensities[i] = I;
+        double m_n = nfg_accumulators[i] / nbg_accumulators[i];
+        variances[i] = std::abs(I) + (std::abs(bg) * (1.0 + m_n));
       }
-      
+      else {
+        success[i] = false;
+      }
     }
+    // Calculate the partiality of all reflections
+    for (int i=0;i<num_reflections;i++){
+      double xyzcal_px_z = phi_column(i,2) * RAD2DEG / osc_width;
+      // then convert to phi
+      // convert bbox start and end to phi.
+
+      double phi = osc_start + ((xyzcal_px_z + 1 - image_range_start)*osc_width);
+      double phia = osc_start + ((computed_bounding_boxes[i].z_min + 1 - image_range_start)*osc_width);
+      double phib = osc_start + ((computed_bounding_boxes[i].z_max + 1 - image_range_start)*osc_width);
+
+      double zeta = coord_system_vector[i].zeta();
+      double c = std::abs(zeta) / (std::sqrt(2.0) * sigma_m);
+      double p = 0.5 * (std::erf(c * (phib - phi)) - std::erf(c * (phia - phi)));
+      partialities[i] = p;
+    }
+
     integrated_data.add_column(
-      "intensity", num_reflections,1,intensities
+      "intensity.sum.value", num_reflections,1,intensities
+    );
+    integrated_data.add_column(
+      "intensity.sum.variance", num_reflections,1,variances
+    );
+    integrated_data.add_column(
+      "partiality", num_reflections, 1, partialities
     );
     integrated_data.add_column(
       "miller_index", num_reflections,3, hkl_vectors
     );
+    std::vector<double> xyzcal_mm = std::vector<double>(
+      phi_column.data_handle(), phi_column.data_handle() + phi_column.size()
+    );
+    std::vector<double> s1 = std::vector<double>(
+      s1_vectors.data_handle(), s1_vectors.data_handle() + s1_vectors.size()
+    );
+    std::vector<int> id = std::vector<int>(num_reflections, 0);
+    integrated_data.add_column("xyzcal.mm", num_reflections, 3, xyzcal_mm);
+    integrated_data.add_column("s1", num_reflections, 3, s1);
+    integrated_data.add_column("id", num_reflections, 1, id);
+    ReflectionTable success_data = integrated_data.select(success);
+    int n_integrated = success_data.column<double>("intensity.sum.value").value().extent(0);
 
-    integrated_data.write(output_file);
+
+    // Other things to output
+    // d, flags, id, intensity.sum.variance, lp, partiality, xyzcal.px?,
+    // xyzobs.mm.value, xyzobs.mm.variance, xyzobs.px.value, xyzobs.px.variance
+
+    success_data.write(output_file);
     logger.info("Baseline integration complete!");
     logger.info("Summary:");
     logger.info("  {} reflections processed", num_reflections);
     logger.info("  {} bounding boxes computed", computed_bounding_boxes.size());
+    logger.info("  {} reflections successfully integrated", n_integrated);
     logger.info("Results saved to: {}", output_file);
 
     return 0;
